f1c338db60b94fada44dddb9974a2d07b7b6eeea
Rhesa Utomo <venderblitz@gmail.com>
2018-07-20 11:46:28 +0700

    change key naming in controller

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var dotenv = require('dotenv').config();
 var cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://amelia:amelia22@ds247698.mlab.com:47698/iamsoinspiring', { useNewUrlParser: true });
+// mongoose.connect('mongodb://amelia:amelia22@ds247698.mlab.com:47698/iamsoinspiring', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/iamsoinspiring', {useNewUrlParser: true})
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
+  console.log('iamsoinspiring connected')
 });
 
 var indexRouter = require('./routes/index');
--- controller/user.js
+++ controller/user.js
@@ -27,23 +27,26 @@ class Users{
     }
 
     static login(req,res){
-        User.find({$or: [{email: req.body.Uname_email}, {username: req.body.Uname_email}]})
+        User.find({$or: [{email: req.body.uname_email}, {username: req.body.uname_email}]})
         .then(user => {
-            if(user.length !=0){
+            if(user.length !== 0){
                 let checkPass = bcrypt.compareSync(req.body.password, user[0].password);
                 if(checkPass){
                     //untuk sementara token taruh di headers, setelah ngerjain client ditaruh di localstorage
                     let token = jwt.sign({ id: user[0]._id, fullname: user[0].full_name , email: user[0].email, image_list: user[0].image_list}, key);
                     res.status(200).json({message:`Happy to see you again ${user[0].full_name}`, token: token})
                 } else {
-                    res.status(400).json('Wrong password')
+                    console.log('wrong pass')
+                    res.status(400).json({error: 'Wrong password'})
                 }
             } else {
-                res.status(400).json('Your email/username is wrong')
+                console.log('wrong uname')
+                res.status(400).json({error: 'Your email/username is wrong'})
             }
         })
         .catch(err => {
-            res.status(500).json(err.message)
+            console.log(err)
+            res.status(500).json({error: err.message})
         })
     }
 
@@ -57,7 +60,7 @@ class Users{
             res.status(200).json(user)
         })
         .catch(err => {
-            res.status(500).json(err.message)
+            res.status(500).json({error: err.message})
         })
     }
 
@@ -82,7 +85,7 @@ class Users{
             res.status(200).json('Account deleted, Bye!')
         })
         .catch(err => {
-            res.status(500).json(err.message)
+            res.status(500).json({error: err.message})
         })
     }
 
@@ -100,21 +103,21 @@ class Users{
                res.status(200).json({status:'Your file is successfully uploaded',image: image})
             })
             .catch(err => {
-                res.status(500).json(err.message)
+                res.status(500).json({error: err.message})
             })
         })
         .catch(err => {
-            res.status(500).json(err.message)
+            res.status(500).json({error: err.message})
         })
     }
 
-    static getImage(req,res){
+    static getImage(req,res) {
         Image.find({})
         .then(image => {
             res.send(image)
         })
         .catch(err => {
-            res.status(500).json(err.message)
+            res.status(500).json({error: err.message})
         })
     }
 }
